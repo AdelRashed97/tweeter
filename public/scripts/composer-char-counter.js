ce73d84e43f7bca3a7ee076b17dd5254a2679a9f
Adel Rashed <adel_rashed@outlook.com>
2020-06-16 17:16:02 -0400

    calculate character length and update counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,10 +1,13 @@
 $(document).ready(function() {
   // --- our code goes here ---
   $("#tweet-text").on("keyup",function(event) {
-    const charLength = ($this).val().length;
-    const counterVal = 124 - charLength;
+    const charLength = $(this).val().length;
+    const counterVal = 140 - charLength;
+    const counter = $(this).closest("form").find(".counter");
+    counter.val(counterVal);
     console.log(counterVal);
+    
 
     
-  })
-})
+  });
+});
