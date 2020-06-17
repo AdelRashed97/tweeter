f6e093d4e279047abd9e40e96b1e310205012478
Adel Rashed <adel_rashed@outlook.com>
2020-06-17 17:05:04 -0400

    built createTweetElement

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,49 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+ $(document).ready(function() {
+  const tweetData = {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png",
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  };
+  
+  const createTweetElement = function(tweetData) {
+    const dateUTC = new Date(0);
+    //const millisecondsUTC = tweetData["created_at"];
+    const tweet = `<article class="tweet">
+    <header>
+      <div class="userInfo">
+        <img src=${tweetData.user.avatars}>
+        <span class="userName">${tweetData.user.name}</span>
+      </div>
+      <span class="userHandler">${tweetData.user.handle}</span>
+    </header>
+    <p> ${tweetData.content.text}</p>
+  
+    <footer>
+     <span class="date">${moment(tweetData["created_at"]).fromNow()} </span>
+     <div class="userReaction">
+      <i class="fas fa-flag fa-xs"></i>
+      <i class="fas fa-retweet fa-xs"></i>
+      <i class="fas fa-heart fa-xs"></i>
+     </div>
+    </footer>
+  
+  </article>`;
+    return tweet;
+  };
+  
+  $("#tweet-container").append(createTweetElement(tweetData));
+  
+
+  $("#tweet-container").append($tweet);
+
+ })
+
